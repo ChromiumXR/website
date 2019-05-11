ccd90b1561f6fd73cf80e08197b515bcc74258b4
Morgan Dean <{ID}+{username}@users.noreply.github.com>
2019-05-10 14:43:49 -1000

    updated da model

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 XRLoader.init({desktop: true, id: 'ha'});
 setInterval(function() {
-    let oldY = parseInt(document.getElementById('r2d2').getAttribute('rotation').split(',')[1]);
-    document.getElementById('r2d2').setAttribute('rotation', '0,' + (oldY + 1) + ',0')
+    let oldY = parseInt(document.getElementById('chrome').getAttribute('rotation').split(',')[1]);
+    document.getElementById('chrome').setAttribute('rotation', '0,' + (oldY + 1) + ',0')
 }, 20);
